83a382ade5b315877deea06deba2436a24c0969b
1452073959@qq.com <1452073959@qq.com>
2020-07-12 22:58:54 +0800

    1

--- app/Admin/Repositories/Category.php
+++ app/Admin/Repositories/Category.php
@@ -13,19 +13,5 @@ class Category extends EloquentRepository
      * @var string
      */
     protected $eloquentClass = CategoryModel::class;
-    public function store(Form $form)
-    {
-        // 获取待新增的数据
-        $attributes = $form->updates();
-        return true;
-    }
 
-    public function update(Form $form)
-    {
-        // 获取待编辑的数据
-        $attributes = $form->updates();
-//        dd($attributes);
-
-        return true;
-    }
 }
